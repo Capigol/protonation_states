7368526cbda88088814fd371ec828b9243492e5a
Lucas Nicolas Alberca <86059300+Capigol@users.noreply.github.com>
2023-08-29 10:37:00 -0300

    Add files via upload

---
+++ protonation_states_streamlit.py
@@ -0,0 +1,277 @@
+"""
+Created on Mon Nov  2 21:32:18 2020
+
+@author: Lideb
+"""
+
+from datetime import date
+import streamlit as st
+import pandas as pd
+import base64
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import DataStructs
+from pathlib import Path
+from PIL import Image
+from openbabel import openbabel
+
+#---------------------------------#
+# Page layout
+## Page expands to full width
+st.set_page_config(page_title='LIDEB Tools - Protonation states',
+    layout='wide')
+
+######
+# Function to put a picture as header   
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+image = Image.open('cropped-header.png')
+st.image(image)
+
+st.write("[![Twitter Follow](https://img.shields.io/twitter/follow/LIDeB_UNLP?style=social)](https://twitter.com/intent/follow?screen_name=LIDeB_UNLP)")
+st.subheader(":pushpin:" "About Us")
+st.markdown("We are a drug discovery team with an interest in the development of publicly available open-source customizable cheminformatics tools to be used in computer-assisted drug discovery. We belong to the Laboratory of Bioactive Research and Development (LIDeB) of the National University of La Plata (UNLP), Argentina. Our research group is focused on computer-guided drug repurposing and rational discovery of new drug candidates to treat epilepsy and neglected tropical diseases.")
+st.markdown(":computer:""**Web Site** " "<https://lideb.biol.unlp.edu.ar>")
+
+#---------------------------------#
+st.write("""
+# Protonation states Web App
+**Che number of clusters to consider.**
+The tool uses the following packages [RDKIT](https://www.rdkit.org/docs/index.html), [Scikit-learn](https://scikit-learn.org/stable/), [Plotly](https://plotly.com/python/), [Scipy](https://www.scipy.org/)
+""")
+
+text = '''
+---
+'''
+
+st.markdown("""
+         **To cite the application, please reference XXXXXXXXX**
+         """)
+
+st.markdown(text)
+
+#---------------------------------#
+# Sidebar - Collects user input features into dataframe
+st.sidebar.header('Upload your datasets SMILES')
+
+uploaded_file_1 = st.sidebar.file_uploader("Upload a TXT file with one SMILES per line", type=["txt"])
+
+# Plot configuration
+config = {'toImageButtonOptions': {'format': "svg", 
+    'height': 800,'width': 1200}}
+
+#%%
+
+ph_user = 7.4
+
+### FP calculation ###
+def ph_function(df_1):
+    list_smiles = df_1[0].tolist()
+    lenght_dataset_1 = len(list_smiles)
+    st.markdown('Dataset has: ' + str(lenght_dataset_1) + " molecules")
+    ready_smiles = []
+    for i,molecula in enumerate(list_smiles):
+        try:
+            smiles_ionized = charges_ph(molecula, ph_user)
+            smile_checked = smile_obabel_corrector(smiles_ionized)
+
+            ready_smiles.append(smile_checked)
+        except:
+            st.write("We have a problem with molecule: " + str(molecula))
+            st.error('Please review your input molecules or remove the above molecule')
+            st.stop()
+    return ready_smiles
+ 
+    
+def charges_ph(molecule, ph):
+
+    # obConversion it's neccesary for saving the objects
+    obConversion = openbabel.OBConversion()
+    obConversion.SetInAndOutFormats("smi", "smi")
+    
+    # create the OBMol object and read the SMILE
+    mol = openbabel.OBMol()
+    obConversion.ReadString(mol, molecule)
+    
+    # Add H, correct pH and add H again, it's the only way it works
+    mol.AddHydrogens()
+    mol.CorrectForPH(7.4)
+    mol.AddHydrogens()
+    
+    # transforms the OBMOl objecto to string (SMILES)
+    optimized = obConversion.WriteString(mol)
+    
+    return optimized
+   
+def smile_obabel_corrector(smiles_ionized):
+    mol1 = Chem.MolFromSmiles(smiles_ionized, sanitize = False)
+    
+    # checks if the ether group is wrongly protonated
+    pattern1 = Chem.MolFromSmarts('[#6]-[#8-]-[#6]')
+    if mol1.HasSubstructMatch(pattern1):
+        # gets the atom number for the O wrongly charged
+        at_matches = mol1.GetSubstructMatches(pattern1)
+        at_matches_list = [y[1] for y in at_matches]
+        # changes the charged for each O atom
+        for at_idx in at_matches_list:
+            atom = mol1.GetAtomWithIdx(at_idx)
+            atom.SetFormalCharge(0)
+            atom.UpdatePropertyCache()
+
+    pattern12 = Chem.MolFromSmarts('[#6]-[#8-]-[#16]')
+    if mol1.HasSubstructMatch(pattern12):
+        # gets the atom number for the O wrongly charged
+        at_matches = mol1.GetSubstructMatches(pattern12)
+        at_matches_list = [y[1] for y in at_matches]
+        # changes the charged for each O atom
+        for at_idx in at_matches_list:
+            atom = mol1.GetAtomWithIdx(at_idx)
+            atom.SetFormalCharge(0)
+            atom.UpdatePropertyCache()
+            
+    # checks if the nitro group is wrongly protonated in the oxygen
+    pattern2 = Chem.MolFromSmarts('[#6][O-]=[N+](=O)[O-]')
+    if mol1.HasSubstructMatch(pattern2):
+        # print('NO 20')
+        patt = Chem.MolFromSmiles('[O-]=[N+](=O)[O-]', sanitize = False)
+        repl = Chem.MolFromSmiles('O[N+]([O-])=O')
+        rms = AllChem.ReplaceSubstructs(mol1,patt,repl,replaceAll=True)
+        mol1 = rms[0]
+
+    # checks if the nitro group is wrongly protonated in the oxygen
+    pattern21 = Chem.MolFromSmarts('[#6]-[O-][N+](=O)=[O-]')
+    if mol1.HasSubstructMatch(pattern21):
+        # print('NO 21')
+        patt = Chem.MolFromSmiles('[O-][N+](=O)=[O-]', sanitize = False)
+        repl = Chem.MolFromSmiles('[O][N+](=O)-[O-]')
+        rms = AllChem.ReplaceSubstructs(mol1,patt,repl,replaceAll=True)
+        mol1 = rms[0]
+        
+    # checks if the nitro group is wrongly protonated, different disposition of atoms
+    pattern22 = Chem.MolFromSmarts('[#8-][N+](=[#6])=[O-]')
+    if mol1.HasSubstructMatch(pattern22):
+        # print('NO 22')
+        patt = Chem.MolFromSmiles('[N+]([O-])=[O-]', sanitize = False)
+        repl = Chem.MolFromSmiles('[N+]([O-])-[O-]')
+        rms = AllChem.ReplaceSubstructs(mol1,patt,repl,replaceAll=True)
+        mol1 = rms[0]
+
+    # checks if the nitro group is wrongly protonated, different disposition of atoms
+    pattern23 = Chem.MolFromSmarts('[#6][N+]([#6])([#8-])=[O-]')
+    if mol1.HasSubstructMatch(pattern23):
+        # print('NO 23')
+        patt = Chem.MolFromSmiles('[N+]([O-])=[O-]', sanitize = False)
+        repl = Chem.MolFromSmiles('[N+]([O-])[O-]')
+        rms = AllChem.ReplaceSubstructs(mol1,patt,repl,replaceAll=True)
+        mol1 = rms[0]
+
+    # checks if the nitro group is wrongly protonated, different disposition of atoms
+    pattern24 = Chem.MolFromSmarts('[#6]-[#8][N+](=O)=[O-]')
+    if mol1.HasSubstructMatch(pattern24):
+        # print('NO 24')
+        patt = Chem.MolFromSmiles('[O][N+](=O)=[O-]', sanitize = False)
+        repl = Chem.MolFromSmiles('[O][N+](=O)[O-]')
+        rms = AllChem.ReplaceSubstructs(mol1,patt,repl,replaceAll=True)
+        mol1 = rms[0]
+
+    # checks if the 1H-tetrazole group is wrongly protonated
+    pattern3 = Chem.MolFromSmarts('[#7]-1-[#6]=[#7-]-[#7]=[#7]-1')
+    if mol1.HasSubstructMatch(pattern3):
+        # gets the atom number for the N wrongly charged
+        at_matches = mol1.GetSubstructMatches(pattern3)
+        at_matches_list = [y[2] for y in at_matches]
+        # changes the charged for each N atom
+        for at_idx in at_matches_list:
+            atom = mol1.GetAtomWithIdx(at_idx)
+            atom.SetFormalCharge(0)
+            atom.UpdatePropertyCache()
+
+    # checks if the 2H-tetrazole group is wrongly protonated
+    pattern4 = Chem.MolFromSmarts('[#7]-1-[#7]=[#6]-[#7-]=[#7]-1')
+    if mol1.HasSubstructMatch(pattern4):
+        # gets the atom number for the N wrongly charged
+        at_matches = mol1.GetSubstructMatches(pattern4)
+        at_matches_list = [y[3] for y in at_matches]
+        # changes the charged for each N atom
+        for at_idx in at_matches_list:
+            atom = mol1.GetAtomWithIdx(at_idx)
+            atom.SetFormalCharge(0)
+            atom.UpdatePropertyCache()
+        
+    # checks if the 2H-tetrazole group is wrongly protonated, different disposition of atoms
+    pattern5 = Chem.MolFromSmarts('[#7]-1-[#7]=[#7]-[#6]=[#7-]-1')
+    if mol1.HasSubstructMatch(pattern5):
+        # gets the atom number for the N wrongly charged
+        at_matches = mol1.GetSubstructMatches(pattern4)
+        at_matches_list = [y[4] for y in at_matches]
+        # changes the charged for each N atom
+        for at_idx in at_matches_list:
+            atom = mol1.GetAtomWithIdx(at_idx)
+            atom.SetFormalCharge(0)
+            atom.UpdatePropertyCache()
+
+    smile_checked = Chem.MolToSmiles(mol1)
+    return smile_checked
+
+
+#%% 
+
+def filedownload2(df):
+    csv = df.to_csv(index=False,header=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="clustering_settings.csv">Download CSV File with your clustering settings</a>'
+    return href
+
+
+#%%
+
+
+if uploaded_file_1 is not None:
+    run = st.button("RUN")
+    if run == True:
+        df_1 = pd.read_csv(uploaded_file_1,sep="\t",header=None)
+        final_smiles = ph_function(df_1)
+        df_1["Optimized_SMILES"] = final_smiles
+        df_1.rename(columns={0:'Input_SMILES', 1:'Acc_global_training',2:"acc_activos",3:"acc_inactivos"}, inplace= True)
+        st.write(df_1)
+
+    else:
+        pass
+# Example file
+else:
+    st.info('Awaiting for TXT file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        df_1 = pd.read_csv("molecules_1.txt",sep="\t",header=None)
+
+
+#Footer edit
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Made in  🐍 and <img style='display: ; ' href="https://streamlit.io" src="https://i.imgur.com/iIOA6kU.png" target="_blank"></img> Developed with ❤️ by <a style='display:; text-align: center;' href="https://lideb.biol.unlp.edu.ar/" target="_blank">LIDeB</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
